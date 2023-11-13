381977586ec67f5402f6aa86f422a6ddd94e135a
Mohamad Hafidz Al Habib <98099534+hafidzalhabib@users.noreply.github.com>
2023-11-13 10:13:33 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import streamlit as st
-from streamlit_webrtc import webrtc_streamer, WebRtcMode
+from streamlit_webrtc import VideoProcessorBase, webrtc_streamer, WebRtcMode, ClientSettings
+from turn import get_ice_servers
+import av
 
 st.set_page_config(
     page_title="push up counter",
@@ -12,7 +14,24 @@ st.set_page_config(
     }
 )
 
-stream = webrtc_streamer(key = "coba", mode=WebRtcMode.SENDRECV,
-                         rtc_configuration={"iceServers": [{"urls": ["stun:stun.l.google.com:19302"]}]
-                         }
-                         )
+class coba(VideoProcessorBase):
+    def recv(self, frame: av.VideoFrame) -> av.VideoFrame:
+        image = frame.to_ndarray(format="bgr24")
+        # image = cv.
+        # cv2.putText(image, f"ANDA TIDAK TERDETEKSI!", (20, 260), cv2.FONT_HERSHEY_DUPLEX, 1.5, (0, 0, 255), 4, cv2.LINE_4)
+        return av.VideoFrame.from_ndarray(image, format="bgr24")
+
+def main():
+    def processor_factory():
+        return coba()
+
+    webrtc_ctx = webrtc_streamer(
+        key="tokyo2020-Pictogram",
+        mode=WebRtcMode.SENDRECV,
+        rtc_configuration={"iceServers": get_ice_servers()},
+        media_stream_constraints={"video": True, "audio": False},
+        video_processor_factory=processor_factory,
+    )
+if __name__ == "__main__":
+    main()
+
